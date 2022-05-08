815f75bd8b4fa151350fbff09db90beeb0aa6013
Cindy Manikowski <mankow58047@gmail.com>
2022-05-08 00:09:59 -0500

    added nav bar with some additional frist page features

--- src/app/weight-mate.service.ts
+++ src/app/weight-mate.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { User } from './user';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 
 @Injectable({
@@ -15,5 +15,11 @@ export class WeightMateService {
     addUser(newUr: User){
       return this.http.post('https://weight-mate-1c908-default-rtdb.firebaseio.com/'+'user.json', newUr);
     }
+
+    getUser(){
+      return this.http.get<User[]>('https://weight-mate-1c908-default-rtdb.firebaseio.com/'+'user.json');
+    }
   
 }
+
+//API key for USDA food search 9nvWyMu1KuPfHcQGPA4ln6tT3e0bk3tR16tpc4Jm
