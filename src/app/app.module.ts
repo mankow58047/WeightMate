57905a1f06e0697c11483d3dc545d0d535c27612
SanjinAgic141 <104841438+SanjinAgic141@users.noreply.github.com>
2022-05-07 20:39:08 -0500

    Added new components and finished setting up entry dashboard. Still needs styling done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { EntryDashboardComponent } from './entry-dashboard/entry-dashboard.component';
+import { DashboardComponent } from './dashboard/dashboard.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    EntryDashboardComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
