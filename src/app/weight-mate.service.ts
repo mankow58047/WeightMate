54af03f68b6436762409f42485d36a0cd889558d
Cindy Manikowski <mankow58047@gmail.com>
2022-05-09 22:01:26 -0500

    commiting prior to adding npm firebase

--- src/app/weight-mate.service.ts
+++ src/app/weight-mate.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { User } from './user';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { userInfo } from 'os';
 
 
 @Injectable({
@@ -13,11 +14,13 @@ export class WeightMateService {
 
   foodAPI: string = '9nvWyMu1KuPfHcQGPA4ln6tT3e0bk3tR16tpc4Jm'
   foodQuery: any = 'chedder cheese'
-  foodURL: string = 'https://api.nal.usda.gov/fdc/v1/foods/search?api_key='
-
-  //foodSearch (){}
-
+  foodURL: string = 'https://api.nal.usda.gov/fdc/v1/foods/search?api_key='+this.foodAPI+'&query'+this.foodQuery
   
+  getFood (){
+   return fetch(this.foodURL).then(Response => Response.json())
+  }
+
+
 
     addUser(newUr: User){
       return this.http.post('https://weight-mate-1c908-default-rtdb.firebaseio.com/'+'user.json', newUr);
