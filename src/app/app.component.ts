0a79bce425aebf0d3902592251a393125f06ed78
SanjinAgic141 <104841438+SanjinAgic141@users.noreply.github.com>
2022-05-10 20:50:27 -0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import { provideDatabase,getDatabase, Database } from '@angular/fire/database';
+import { ExerciseService } from './exercise.service';
+import { FoodService } from './food.service';
+import { WaterService } from './water.service';
+import { WeightService } from './weight.service';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ExerciseService, FoodService, WaterService, WeightService],
 })
 export class AppComponent {
   title = 'WeightMate';
+
+  
+  showFood() {
+    this.foodService.display = 1;
+  }
+
+  showWater() {
+    this.waterService.display = 2;
+  }
+
+  showWeight() {
+    this.weightService.display = 3;
+  }
+
+  showExercise() {
+    this.exerciseService.display = 4;
+  }
 
 }
 
